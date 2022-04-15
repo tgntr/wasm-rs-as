f391b4f2e849314cc2081a4d50fc520726e6e5f8
Tugay Emin <tgntrr@gmail.com>
2022-04-15 13:31:46 +0300

    cleanup

--- assembly/contract.ts
+++ assembly/contract.ts
@@ -1,7 +1,16 @@
+import { allocate, Region } from "./memory";
+import * as env from "./env";
 export { allocate, deallocate } from "./memory";
 
 export function instantiate(env_ptr: usize, info_ptr: usize, msg_ptr: usize): usize {
-    return 0
+    // Uint8Array constructor uses __new and throws
+    // new Uint8Array(250);
+    let region = changetype<Region>(allocate(250));
+    region.offset = changetype<usize>('{"ok":{"messages":[],"attributes":[],"events":[],"data":null}}');
+    region.length = 250;
+    let ptr = changetype<usize>(region);
+    env.debug(ptr);
+    return ptr;
 }
 
 export function execute(env_ptr: usize, info_ptr: usize, msg_ptr: usize): usize {
