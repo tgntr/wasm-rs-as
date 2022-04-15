e2cf1faa6a2f1e1b6f32926ed2714d779ca89a03
Tugay Emin <tgntrr@gmail.com>
2022-04-15 13:33:38 +0300

    cleanup

--- assembly/env.ts
+++ assembly/env.ts
@@ -1,8 +1,1 @@
-// fn debug(&self, message: &str) {
-//     // keep the boxes in scope, so we free it at the end (don't cast to pointers same line as build_region)
-//     let region = build_region(message.as_bytes());
-//     let region_ptr = region.as_ref() as *const Region as u32;
-//     unsafe { debug(region_ptr) };
-// 
-
 export declare function debug(ptr: usize): void;
